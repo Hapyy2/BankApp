99cd55e4394544bfd8bbed81aff1a4ea40116af4
Mateusz <mateusz.klemann@wp.pl>
2024-10-30 12:51:56 +0100

    feature_4_finished

--- app/Konto.py
+++ app/Konto.py
@@ -1,10 +1,17 @@
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, imie, nazwisko, pesel, promo = ""):
         self.imie = imie
         self.nazwisko = nazwisko
+        self.pesel = pesel
         self.saldo = 0
-        if len(pesel) != 11:
-            self.pesel = "Niepoprawny pesel!"
-        else:    
-            self.pesel = pesel
+        self.promo = promo
+        self.test_pesel()
+        self.test_kodu(promo)
 
+    def test_pesel(self):
+        if len(self.pesel) != 11:
+            self.pesel = "Niepoprawny pesel!"
+
+    def test_kodu(self, kod):
+        if kod[0:5] == "PROM_" and len(kod) == 8:
+            self.saldo = 50
