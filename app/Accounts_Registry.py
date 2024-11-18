229a860fd6e3f9acc4b3fdb738c18a6ef0d5149c
Mateusz <mateusz.klemann@wp.pl>
2024-11-18 17:40:25 +0100

    poprawki

--- app/Accounts_Registry.py
+++ app/Accounts_Registry.py
@@ -1,15 +1,17 @@
 class Accounts_Registry():
-    def __init__(self):
-        self.registry = []
+    registry = []
     
-    def AddAccount(self, account):
-        self.registry.append(account)
+    @classmethod
+    def AddAccount(cls, account):
+        cls.registry.append(account)
     
-    def SearchAccount(self, test_pesel):
-        for person in self.registry:
+    @classmethod
+    def SearchAccount(cls, test_pesel):
+        for person in cls.registry:
             if person.pesel == test_pesel:
                 return person
         return "Nie znaleziono konta z podanym peselem"
 
-    def CountAccount(self):
-        return len(self.registry)
+    @classmethod
+    def CountAccount(cls):
+        return len(cls.registry)
--- app/tests/test_registry.py
+++ app/tests/test_registry.py
@@ -5,38 +5,28 @@ from app.Konto_osobiste import *
 from parameterized import *
 
 class TestRegistry(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.konto_os = Konto_osobiste("Jan", "Kowalski", "12345678901")
+
     def setUp(self):
-        self.konto_os = Konto_osobiste("Jan", "Kowalski", "12345678901")
-        self.test_reg = Accounts_Registry()
+        Accounts_Registry.registry = []
     
     def test_add_acc(self):
-        self.test_reg.AddAccount(self.konto_os)
-        self.assertEqual(self.test_reg.registry[-1], self.konto_os, "Nie dodano konta do rejestru")
-
-    # @parameterized.expand([
-    #     ("test_znaleziono", "Błąd", "11122233344", "Nie znaleziono konta z podanym peselem"),
-    #     ("test_nie_znaleziono", "Nie znaleziono konta z podanym peselem a powinno", "12345678901", self.konto_os)
-
-    # ])
-    # def test_search_acc(self, name, error, test_pesel, result):
-    #     print(f"Test: {name}")
-
-    #     self.test_reg.AddAccount(self.konto_os)
-    #     searched = self.test_reg.SearchAccount(test_pesel)
-
-    #     self.assertEqual(searched, result, f"Error: {error}")
+        Accounts_Registry.AddAccount(self.konto_os)
+        self.assertEqual(Accounts_Registry.registry[-1], self.konto_os, "Nie dodano konta do rejestru")
 
     def test_search_acc_found(self):
-        self.test_reg.AddAccount(self.konto_os)
-        searched = self.test_reg.SearchAccount("11122233344")
+        Accounts_Registry.AddAccount(self.konto_os)
+        searched = Accounts_Registry.SearchAccount("11122233344")
         self.assertEqual(searched, "Nie znaleziono konta z podanym peselem", f"Error: Błąd")
     
     def test_search_acc_not_found(self):
-        self.test_reg.AddAccount(self.konto_os)
-        searched = self.test_reg.SearchAccount("12345678901")
+        Accounts_Registry.AddAccount(self.konto_os)
+        searched = Accounts_Registry.SearchAccount("12345678901")
         self.assertEqual(searched, self.konto_os, f"Error: Nie znaleziono konta z podanym peselem a powinno")
 
     def test_count_acc(self):
-        self.test_reg.AddAccount(self.konto_os)
-        count = self.test_reg.CountAccount()
+        Accounts_Registry.AddAccount(self.konto_os)
+        count = Accounts_Registry.CountAccount()
         self.assertEqual(count, 1, "Nieprawidłowo policzono ilość kont")
